cded74384c8e59d866891d89b65706c37a9ac18d
Agustin Salum <agustinsalum92@hotmail.com>
2024-03-27 17:49:25 -0300

    Se dividieron los grid para los proyectos

--- assets/js/keywords.js
+++ assets/js/keywords.js
@@ -1,6 +1,6 @@
 
 var keywords = ["Programmer Analyst ", "Developer ", "Software Engineer ", "Web developer ", "Data Analyst ", "Freelance "];
-    
+
 // Counter of the current word
 var index = 0;
 
@@ -8,20 +8,20 @@ var index = 0;
 function animateKeywords() {
     // Searches the HTML for the id 'keywords' attribute
     var keywordElement = document.getElementById('keywords');
-    
+
     // If the index is less than the length of the array, continue the animation
     if (index < keywords.length) {
         var currentKeyword = keywords[index];
         var i = 0;
         // Sets a timer that repeatedly executes a function or a block of code every certain specified time interval
-        var interval = setInterval(function() {
+        var interval = setInterval(function () {
             // Check if we have reached the last letter of the current word
             if (i < currentKeyword.length - 1) {
                 keywordElement.textContent += currentKeyword[i];
                 i++;
             } else {
                 clearInterval(interval);
-                setTimeout(function() {
+                setTimeout(function () {
                     eraseKeyword(keywordElement);
                 }, 1000); // Time to wait before deleting the word
             }
@@ -31,7 +31,7 @@ function animateKeywords() {
 
 // Function to delete the current word
 function eraseKeyword(element) {
-    var interval = setInterval(function() {
+    var interval = setInterval(function () {
         element.textContent = element.textContent.slice(0, -1);
         // If the entire word has been deleted
         if (element.textContent === '') {
@@ -39,13 +39,13 @@ function eraseKeyword(element) {
             index++;
             // If there are words left to animate, start the animation of the next word
             if (index < keywords.length) {
-                setTimeout(function() {
+                setTimeout(function () {
                     animateKeywords();
                 }, 500); // Timeout before displaying next word
             } else {
                 // Reset the index to start the animation again
                 index = 0;
-                setTimeout(function() {
+                setTimeout(function () {
                     animateKeywords();
                 }, 500); // Time to wait before displaying the first word
             }
